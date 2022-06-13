cc246568a42ab40c47a9eb5097fe5708fd4de06d
DESKTOP-MR44V0P\Satya <satya0906@gmail.com>
2022-06-13 17:03:33 +0700

    first commit

---
+++ migrations/20220613093007-create-table-refresh-tokens.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+   
+     /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('refresh-tokens', { id: Sequelize.INTEGER });
+     */
+     
+  },
+
+  async down (queryInterface, Sequelize) {
+    
+     /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+     
+  }
+};
