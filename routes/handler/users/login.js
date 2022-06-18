462ee1d2fbae11b3acbf68ab15b3e1e018109288
DESKTOP-MR44V0P\Satya <satya0906@gmail.com>
2022-06-19 00:07:56 +0700

    Service Users : API Login DONE

---
+++ routes/handler/users/login.js
@@ -0,0 +1,70 @@
+const bcrypt = require('bcrypt');
+const {User} = require('../../../models');
+
+const Validator = require('fastest-validator');
+const v = new Validator();
+
+
+module.exports = async (req, res) => {
+    const schema = {        
+        email : 'email|empty:false',
+        password : 'string|min:6',        
+    };
+    
+    const validate = v.validate(req.body, schema); // validate user input
+
+    if (validate.length){
+        // user inputs do not pass validation
+        return res.status(400).json(
+            {
+                status : "Error",
+                message : validate,
+            }
+        ); 
+    }else{
+        // user input pass validation
+
+        // check email already exists not 
+        const user = await User.findOne({
+            where : {email : req.body.email,},
+        });
+
+        if (!user){
+            // Email has not been registered
+            return res.status(404).json({
+                status : "Error",
+                message : "User not found",
+            });
+        }else {
+            // Email has been registred.
+
+            // Check the password matched or not.
+            const isValidPassword = await bcrypt.compare(req.body.password, user.password);
+
+            if(!isValidPassword) {
+                // Password not matched
+                return res.status(404).json({
+                    status : "Error",
+                    message : "Password not matched",
+                });
+            }else{
+                return res.status(200).json({
+                    status : "Success",
+                    message : "Login successful",
+                    data : {
+                        id : user.id,
+                        name : user.name,
+                        email : user.email,
+                        role : user.role,
+                        avatar : user.avatar,
+                        profession : user.profession,
+                    }
+                });
+            }
+
+
+        }
+    }
+
+
+}
--- routes/users.js
+++ routes/users.js
@@ -5,6 +5,8 @@ var userHandler = require('./handler/users');
 /* GET users listing. */
 router.post('/register', userHandler.register);
 
+router.post('/login', userHandler.login);
+
 
 
 module.exports = router;
